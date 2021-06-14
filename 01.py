ea1739627a7efc863e9cec80fd2f3f1bb3f75acc
n-ebrahimian <82975802+n-ebrahimian@users.noreply.github.com>
2021-06-14 17:25:53 +0430

    modified

--- 01.py
+++ 01.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 rows = 320
 cols = 320
 
-chess_board = np.zeros((rows, cols))
+chess_board = np.zeros((rows, cols), dtype = 'uint8')
 block_size = chess_board.shape[0] // 8
 
 flag = 0
@@ -15,10 +15,11 @@ for i in range(0, rows, block_size):
     for j in range(0, cols, block_size):
 
         if flag == 0:
+            chess_board[i : i + block_size, j : j + block_size] = 0
             flag = 1
 
         else:    
-            chess_board[i : i + block_size, j : j + block_size] = 1
+            chess_board[i : i + block_size, j : j + block_size] = 255
             flag = 0 
             
     if flag == 0:
@@ -28,3 +29,4 @@ for i in range(0, rows, block_size):
       flag = 0
 
 plt.imshow(chess_board, cmap = 'gray')
+cv2.imwrite("/content/drive/MyDrive/Assignment-21/Results/chess_board.jpg", chess_board)
