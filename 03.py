0de3c910350b326cf6f4de544c8d19168c540124
n-ebrahimian <82975802+n-ebrahimian@users.noreply.github.com>
2021-06-14 16:39:42 +0430

    Add files via upload

--- 03.py
+++ 03.py
@@ -2,8 +2,17 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-image3 = cv2.imread("/content/drive/MyDrive/Assignment-21/3.jpg")
+image3 = cv2.imread("3.jpg")
 
-image3 = cv2.rotate(image3, cv2.ROTATE_180)
-plt.imshow(image3)
-cv2.imwrite("/content/drive/MyDrive/Assignment-21/Results/03new.jpg", image3)
+new_image3 = np.zeros_like(image3)
+
+rows = new_image3.shape[0]
+cols = new_image3.shape[1]
+
+for i in range(new_image3.shape[0]):
+  for j in range(new_image3.shape[1]):
+    
+    new_image3[i, j] = image3[rows - 1 - i, cols - 1 - j]
+
+plt.imshow(new_image3, cmap = 'gray')
+cv2.imwrite("/content/drive/MyDrive/Assignment-21/Results/03new.jpg", new_image3)
