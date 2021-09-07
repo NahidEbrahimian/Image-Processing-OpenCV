52a96ccbe857e46b6c4bfcb385fe7ce7858c96f0
Nahid Ebrahimian <82975802+NahidEbrahimian@users.noreply.github.com>
2021-09-07 10:24:01 -0700

    init

---
+++ 01- Pre processing Techniques 01/01.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+rows = 320
+cols = 320
+
+chess_board = np.zeros((rows, cols), dtype = 'uint8')
+block_size = chess_board.shape[0] // 8
+
+flag = 0
+
+for i in range(0, rows, block_size):  
+    for j in range(0, cols, block_size):
+
+        if flag == 0:
+            chess_board[i : i + block_size, j : j + block_size] = 0
+            flag = 1
+
+        else:    
+            chess_board[i : i + block_size, j : j + block_size] = 255
+            flag = 0 
+            
+    if flag == 0:
+      flag = 1
+
+    else:
+      flag = 0
+
+plt.imshow(chess_board, cmap = 'gray')
+cv2.imwrite("/content/drive/MyDrive/Assignment-21/Results/chess_board.jpg", chess_board)
---
+++ 01- Pre processing Techniques 01/README.md
@@ -0,0 +1,13 @@
+# Results
+
+# ![chess_board](https://github.com/n-ebrahimian/Image-processing-techniques/blob/main/Pre-processing-techniques-01/Inputs/chess_board.jpg)
+
+# ![01new](https://github.com/n-ebrahimian/Image-processing-techniques/blob/main/Pre-processing-techniques-01/Results/01new.jpg)
+
+# ![02new](https://github.com/n-ebrahimian/Image-processing-techniques/blob/main/Pre-processing-techniques-01/Results/02new.jpg)
+
+# ![03new](https://github.com/n-ebrahimian/Image-processing-techniques/blob/main/Pre-processing-techniques-01/Results/03new.jpg)
+
+# ![04new](https://github.com/n-ebrahimian/Image-processing-techniques/blob/main/Pre-processing-techniques-01/Results/04.jpg)
+
+
